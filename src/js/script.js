66d48109d78e325968be60888cc5ce69e0c45b5a
Oskar Pozorski <oskar.pozorski@gmail.com>
2023-05-16 23:50:55 +0200

    processOrdr works

--- src/js/script.js
+++ src/js/script.js
@@ -127,6 +127,22 @@ const select = {
     initOrderForm(){
       const thisProduct = this;
       console.log('initOrderForm', thisProduct)
+
+      thisProduct.form.addEventListener('submit', function(event){
+        event.preventDefault();
+        thisProduct.processOrder();
+      });
+      
+      for(let input of thisProduct.formInputs){
+        input.addEventListener('change', function(){
+          thisProduct.processOrder();
+        });
+      }
+      
+      thisProduct.cartButton.addEventListener('click', function(event){
+        event.preventDefault();
+        thisProduct.processOrder();
+      });
     }
 
     processOrder() {
