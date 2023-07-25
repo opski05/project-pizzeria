b2da272b66bde26c74dbc2dfd9831e9a04efb5b8
Oskar Pozorski <oskar.pozorski@gmail.com>
2023-07-25 19:53:27 +0200

    Add fetch to app.initData

--- src/js/script.js
+++ src/js/script.js
@@ -71,6 +71,11 @@ const select = {
     cart: {
       defaultDeliveryFee: 20,
     },
+    db: {
+      url: '//localhost:3131',
+      products: 'products',
+      orders: 'orders',
+    },
   };
 
   const templates = {
@@ -564,7 +569,25 @@ const select = {
     initData: function(){
       const thisApp = this;
   
-      thisApp.data = dataSource;
+      thisApp.data = {};
+
+      const url = settings.db.url + '/' + settings.db.products;
+
+      fetch(url)
+       .then(function(rawResponse){
+          return rawResponse.json();
+        })
+       .then(function(parsedResponse){
+          console.log(parsedResponse)
+
+          /*save parsedResponse as thisApp.data.products */
+          thisApp.data.products = parsedResponse;
+
+          /*execute initMenu method */
+          thisApp.initMenu();
+        });
+      
+        console.log('thisApp.data' , JSON.stringify(thisApp.data));
     },
 
     initMenu: function(){
@@ -572,7 +595,7 @@ const select = {
       console.log('thisApp.data:', thisApp.data);
 
       for(let productData in thisApp.data.products){
-        new Product(productData, thisApp.data.products[productData]);
+        new Product(thisApp.data.products[productData].id, thisApp.data.products[productData]);
       }
     },
     
@@ -585,7 +608,6 @@ const select = {
       console.log('templates:', templates);
 
       thisApp.initData();
-      thisApp.initMenu();
       thisApp.initCart();
     },
 
